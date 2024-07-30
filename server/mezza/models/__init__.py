53cc6d174f56fa4d99bb0541e1aa025b1c40c360
Karl Hobley <karl@kaed.uk>
2024-07-30 22:42:56 +0000

    Add file models

--- server/mezza/models/__init__.py
+++ server/mezza/models/__init__.py
@@ -2,14 +2,21 @@ from .content import (
     Component,
     Content,
     ContentType,
+    LongFormText,
     RepeatableComponent,
 )
+from .files import AudioFile, BaseFile, DocumentFile, ImageFile, VideoFile
 from .workspaces import Workspace
 
 __all__ = [
     "ContentType",
     "Content",
     "Component",
+    "BaseFile",
+    "ImageFile",
+    "VideoFile",
+    "AudioFile",
+    "DocumentFile",
     "LongFormText",
     "RepeatableComponent",
     "Workspace",
