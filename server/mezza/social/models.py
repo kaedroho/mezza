bfe7f3426097af34f52c8409fcf63a84cfd3d0fc
Karl Hobley <karl@kaed.uk>
2024-08-04 17:56:09 +0000

    Add Posting model

--- server/mezza/social/models.py
+++ server/mezza/social/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-from ..models.content import Component
+from ..models.content import Component, Content
 from ..models.workspaces import Workspace
 
 __all__ = [
+    "Channel",
+    "Posting",
     "BasePost",
     "XPost",
     "FacebookPost",
@@ -16,8 +18,21 @@ class Channel(models.Model):
     workspace = models.ForeignKey(Workspace, on_delete=models.PROTECT)
 
 
+class Posting(models.Model):
+    content = models.ForeignKey(
+        Content, on_delete=models.CASCADE, related_name="postings"
+    )
+    channel = models.ForeignKey(
+        Channel, on_delete=models.CASCADE, related_name="postings"
+    )
+    posted_at = models.DateTimeField()
+
+    class Meta:
+        unique_together = [("content", "channel")]
+
+
 class BasePost(Component):
-    channel = models.ForeignKey(Channel, on_delete=models.PROTECT)
+    pass
 
     class Meta:
         abstract = True
