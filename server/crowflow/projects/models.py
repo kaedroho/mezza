4613485dde9aa68fa11c32065752a3fc991a1b0a
Karl Hobley <karl@kaed.uk>
2024-08-15 18:21:39 +0000

    Fill out Project model

--- server/crowflow/projects/models.py
+++ server/crowflow/projects/models.py
@@ -1,15 +1,32 @@
 from django.db import models
 
+from crowflow.files.models import ImageFile, VideoFile
+from crowflow.flows.models import Flow, Stage
+
 __all__ = [
     "Project",
 ]
 
 
 class Project(models.Model):
+    flow = models.ForeignKey(Flow, on_delete=models.CASCADE, related_name="projects")
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE, related_name="projects")
     title = models.TextField(max_length=200)
-    flow = models.ForeignKey(
-        "crowflowflows.Flow", on_delete=models.CASCADE, related_name="projects"
+    due_date = models.DateField(null=True, blank=True)
+    thumbnail = models.ForeignKey(
+        ImageFile, on_delete=models.SET_NULL, related_name="+", null=True
     )
-    stage = models.ForeignKey(
-        "crowflowflows.Stage", on_delete=models.CASCADE, related_name="projects"
+    description = models.TextField(blank=True)
+    script = models.TextField(blank=True)
+    final_video = models.ForeignKey(
+        VideoFile, on_delete=models.SET_NULL, related_name="+", null=True
     )
+
+
+class Footage(models.Model):
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name="footage"
+    )
+    video = models.ForeignKey(VideoFile, on_delete=models.CASCADE, related_name="+")
+    order = models.IntegerField()
+    description = models.TextField()
