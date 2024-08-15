1acd3d2db1a1fef4b5721e217f589dd3d13554cd
Karl Hobley <karl@kaed.uk>
2024-08-15 18:04:58 +0000

    New models for flows/projects

--- server/crowflow/flows/models.py
+++ server/crowflow/flows/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from crowflow.spaces.models import Space
 
 __all__ = [
-    "Flow",
     "Stage",
+    "Flow",
 ]
 
 
@@ -12,7 +16,6 @@ class Stage(models.Model):
     space = models.ForeignKey(
         "crowflowspaces.Space", on_delete=models.CASCADE, related_name="stages"
     )
-    is_custom = models.BooleanField(default=True)
 
 
 class Flow(models.Model):
@@ -20,15 +23,43 @@ class Flow(models.Model):
     space = models.ForeignKey(
         "crowflowspaces.Space", on_delete=models.CASCADE, related_name="flows"
     )
-    stages = models.ManyToManyField("Stage", through="FlowStage", related_name="flows")
-    initial_stage = models.ForeignKey(
-        "Stage", on_delete=models.CASCADE, related_name="+"
-    )
+    stages = models.ManyToManyField("Stage", related_name="flows")
 
 
-class FlowStage(models.Model):
-    flow = models.ForeignKey("Flow", on_delete=models.CASCADE, related_name="+")
-    stage = models.ForeignKey("Stage", on_delete=models.CASCADE, related_name="+")
+@receiver(post_save, sender=Space)
+def create_default_flows(sender, instance, created, **kwargs):
+    if created:
+        idea_stage = Stage.objects.create(title="Idea", order=1, space=instance)
+        todo_stage = Stage.objects.create(title="Todo", order=2, space=instance)
+        scripting_stage = Stage.objects.create(
+            title="Scripting", order=3, space=instance
+        )
+        filming_stage = Stage.objects.create(title="Filming", order=4, space=instance)
+        editing_stage = Stage.objects.create(title="Editing", order=5, space=instance)
+        ready_to_publish_stage = Stage.objects.create(
+            title="Ready to publish", order=6, space=instance
+        )
+        scheduled_stage = Stage.objects.create(
+            title="Scheduled", order=7, space=instance
+        )
+        published_stage = Stage.objects.create(
+            title="Published", order=8, space=instance
+        )
 
-    class Meta:
-        unique_together = ("flow", "stage")
+        videos_flow = Flow.objects.create(
+            title="Videos",
+            space=instance,
+        )
+
+        videos_flow.stages.set(
+            [
+                idea_stage,
+                todo_stage,
+                scripting_stage,
+                filming_stage,
+                editing_stage,
+                ready_to_publish_stage,
+                scheduled_stage,
+                published_stage,
+            ]
+        )
