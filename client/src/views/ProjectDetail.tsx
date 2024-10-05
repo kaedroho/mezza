7ba30b2798678a4e62731de831af45b886367dfb
Karl Hobley <karl@kaed.uk>
2024-10-05 14:03:35 +0000

    Remove form from project detail view

--- client/src/views/ProjectDetail.tsx
+++ client/src/views/ProjectDetail.tsx
@@ -1,4 +1,4 @@
-import { Form, NavigationContext } from "@django-bridge/react";
+import { NavigationContext } from "@django-bridge/react";
 import { Add } from "@mui/icons-material";
 import { Box, Typography } from "@mui/joy";
 import Button from "@mui/joy/Button";
@@ -6,7 +6,6 @@ import React from "react";
 import AssetList from "../components/AssetList";
 import Layout from "../components/Layout";
 import ModalWindow from "../components/ModalWindow";
-import { CSRFTokenContext } from "../contexts";
 import { Asset, Project } from "../types";
 
 interface ProjectDetailViewProps {
@@ -19,74 +18,65 @@ export default function ProjectDetailView({
   assets,
 }: ProjectDetailViewProps) {
   const { openOverlay, refreshProps } = React.useContext(NavigationContext);
-  const csrfToken = React.useContext(CSRFTokenContext);
 
   return (
     <Layout title={project.title} noIndent>
-      <Form action={project.detail_url} method="post">
-        <input type="hidden" name="csrfmiddlewaretoken" value={csrfToken} />
-
-        <Box px={2}>
-          <section>
-            <Typography level="h4">Basic Information</Typography>
-            <Button
-              variant="plain"
-              color="primary"
-              size="sm"
-              startDecorator={<Add />}
-              onClick={() =>
-                openOverlay(
-                  project.edit_url,
-                  (content) => (
-                    <ModalWindow slideout="right">{content}</ModalWindow>
-                  ),
-                  {
-                    onClose: () => {
-                      // Refresh props so new post pops up in listing
-                      refreshProps();
-                    },
+      <Box px={2}>
+        <section>
+          <Typography level="h4">Basic Information</Typography>
+          <Button
+            variant="plain"
+            color="primary"
+            size="sm"
+            startDecorator={<Add />}
+            onClick={() =>
+              openOverlay(
+                project.edit_url,
+                (content) => (
+                  <ModalWindow slideout="right">{content}</ModalWindow>
+                ),
+                {
+                  onClose: () => {
+                    // Refresh props so new post pops up in listing
+                    refreshProps();
                   },
-                )
-              }
-            >
-              Edit
-            </Button>
-            {project.description}
-            {project.release_date}
-          </section>
-
-          <section>
-            <Typography level="h4">Assets</Typography>
-            <Button
-              variant="plain"
-              color="primary"
-              size="sm"
-              startDecorator={<Add />}
-              onClick={() =>
-                openOverlay(
-                  project.asset_upload_url,
-                  (content) => (
-                    <ModalWindow slideout="right">{content}</ModalWindow>
-                  ),
-                  {
-                    onClose: () => {
-                      // Refresh props so new post pops up in listing
-                      refreshProps();
-                    },
-                  },
-                )
-              }
-            >
-              Upload
-            </Button>
-            <AssetList assets={assets} />
-          </section>
-
-          <Button color="primary" type="submit">
-            Save
+                },
+              )
+            }
+          >
+            Edit
           </Button>
-        </Box>
-      </Form>
+          {project.description}
+          {project.release_date}
+        </section>
+
+        <section>
+          <Typography level="h4">Assets</Typography>
+          <Button
+            variant="plain"
+            color="primary"
+            size="sm"
+            startDecorator={<Add />}
+            onClick={() =>
+              openOverlay(
+                project.asset_upload_url,
+                (content) => (
+                  <ModalWindow slideout="right">{content}</ModalWindow>
+                ),
+                {
+                  onClose: () => {
+                    // Refresh props so new post pops up in listing
+                    refreshProps();
+                  },
+                },
+              )
+            }
+          >
+            Upload
+          </Button>
+          <AssetList assets={assets} />
+        </section>
+      </Box>
     </Layout>
   );
 }
